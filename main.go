db99e66074dfb622873f86f41fa9a5ef5e1d0a75
Robert Kroeger <rjkroege@chromium.org>
2015-05-04 07:50:32 -0700

    Added the start of some kind of rational usage

--- main.go
+++ main.go
@@ -1,16 +1,32 @@
 package main
 
 import (
-	"os"
+	"flag"
+	"fmt"
 	"log"
+	"os"
 
 	"github.com/rjkroege/leap/output"
 	"github.com/rjkroege/leap/search"
 )
 
+// TODO(rjk): Add the necessary flags to configure the project
+// root and what not, force re-indexing etc.
+var (
+	ip = flag.Int("flagname", 1234, "help message for flagname")
+)
 
 func main() {
-	log.Printf("hi there")
+	flag.Usage = func() {
+	        fmt.Fprintf(os.Stderr, "Usage of %s:\n", os.Args[0] )
+	        fmt.Fprintf(os.Stderr, "	%s <flags listed below> <search string>\n", os.Args[0] )
+	        flag.PrintDefaults()
+	}
+
+	flag.Parse()
+
+	log.Printf("hi there %d, %v", *ip, flag.Arg(0))
+	
 
 	gen := search.NewStaticGenerator()
 
