75019d000e65eae0e6146317438c73170c27a4f7
Robert Kroeger <rjkroege@chromium.org>
2015-05-31 19:34:45 -0700

    enumerated successfully

--- search/filenames.go
+++ search/filenames.go
@@ -39,14 +39,18 @@ func (ix *filenameSearch) Query(fn, qtype, suffix string) ([]output.Entry, error
 	// use the query-all : var allQuery = &Query{Op: QAll}
 	// then, iterate...
 
-	// OK. 
-	//       for each file name, stuff result into array.
-	for i := 0;  true ; i++ {
-		name := ix.innerQuery(i)
-		log.Printf("id %d %#v", i, name)
-		if name == "\000" || name == "" {
-			break
-		}
+	allQuery := &index.Query{Op: index.QAll}
+	post := ix.PostingQuery(allQuery)
+	
+//	fnames := make([]uint32, 0, len(post))
+
+	for _, fileid := range post {
+		name := ix.Name(fileid)
+		log.Printf("name %#v", name)
+//		if fre.MatchString(name, true, true) < 0 {
+//			continue
+//		}
+//		fnames = append(fnames, fileid)
 	}
 	return nil, nil
 }
