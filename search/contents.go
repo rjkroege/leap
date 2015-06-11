a23b2c833b2d803b26051bbf1d87763247c10869
Robert Kroeger <rjkroege@chromium.org>
2015-06-11 09:32:57 -0700

    cleanup, styling in search, 2nd patch

--- search/contents.go
+++ search/contents.go
@@ -11,23 +11,24 @@ import (
 	"github.com/rjkroege/leap/output"
 )
 
-
 const MaximumMatches = 50
 
 type trigramSearch struct {
 	index.Index
 }
 
+// NewTrigramSearch returns a Generator that can search
+// inside of files.
 func NewTrigramSearch() output.Generator {
-	return &trigramSearch{ *index.Open(index.File()) }
+	return &trigramSearch{*index.Open(index.File())}
 }
 
 func (ix *trigramSearch) Query(fn, qtype, suffix string) ([]output.Entry, error) {
-	// Don't know how to deal with other types other than line-no mode.
-	log.Printf("Query: %#v", qtype)
+	// TODO(rjk): Refactor this code.
 	if qtype == ":" {
 		return ix.fileQuery(fn, qtype, suffix)
 	}
+	log.Printf("Query: %#v", qtype)
 
 	// compile the in-file regexp
 	pat := "(?m)" + suffix
@@ -45,7 +46,7 @@ func (ix *trigramSearch) Query(fn, qtype, suffix string) ([]output.Entry, error)
 
 	q := index.RegexpQuery(re.Syntax)
 	post := ix.PostingQuery(q)
-	
+
 	fnames := make([]uint32, 0, len(post))
 
 	for _, fileid := range post {
@@ -58,31 +59,28 @@ func (ix *trigramSearch) Query(fn, qtype, suffix string) ([]output.Entry, error)
 		fnames = append(fnames, fileid)
 	}
 
-
-	log.Printf("finished the trigram query")
-
 	matches := multiFile(fnames, re, ix)
 
-	oo := make([]output.Entry,0, len(matches))
+	oo := make([]output.Entry, 0, len(matches))
 
-	// TODO(rjk): Do a better job of handling long strings.
+	// TODO(rjk): Do a better job of displaying long match strings.
+	// In particular, preserve the function name in function matches.
 	for _, m := range matches {
 		name := m.fn
-		// It would be nice if Alfred supported styled strings?
+		// It would be nice if Alfred supported styled strings. Then, I
+		// could highlight the search results.
 		title := fmt.Sprintf("%s:%d %s", filepath.Base(name), m.lineno, m.matchLine)
-		
+
 		oo = append(oo, output.Entry{
-			Uid: name +  "/" + m.matchLine,
-			Arg: fmt.Sprintf("%s:%d", name, m.lineno),
-			Title: title,
-			SubTitle:	fmt.Sprintf("%s:%d %s", ix.trimmer(name), m.lineno, m.matchLine),
-			Type: "file",
+			Uid:      name + "/" + m.matchLine,
+			Arg:      fmt.Sprintf("%s:%d", name, m.lineno),
+			Title:    title,
+			SubTitle: fmt.Sprintf("%s:%d %s", ix.trimmer(name), m.lineno, m.matchLine),
+			Type:     "file",
 			Icon: output.AlfredIcon{
 				Filename: "blah.png",
 			},
 		})
-
-		log.Printf("matched %#v", name)
 	}
 
 	return oo, nil
