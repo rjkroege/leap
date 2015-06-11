845da47d0d7552d6d743b3dbd1e5f58e5d1fe234
Robert Kroeger <rjkroege@chromium.org>
2015-06-10 19:46:39 -0700

    search formatting and cleanup, part 1

--- search/static.go
+++ search/static.go
@@ -9,35 +9,33 @@ import (
 
 type staticGenerator []output.Entry
 
-
-
 func NewStaticGenerator() output.Generator {
-	return staticGenerator( []output.Entry{
+	return staticGenerator([]output.Entry{
 		output.Entry{
-			Uid: "foo1",
-			Arg: "anArg",
-			Type: "type1"	,
-			Title: "Title",
+			Uid:      "foo1",
+			Arg:      "anArg",
+			Type:     "type1",
+			Title:    "Title",
 			SubTitle: "the subtitle",
 			Icon: output.AlfredIcon{
 				Filename: "blah.png",
 			},
 		},
 		output.Entry{
-			Uid: "foo2",
-			Arg: "anDifferentArg",
-			Type: "type2"	,
-			Title: "Another Title",
+			Uid:      "foo2",
+			Arg:      "anDifferentArg",
+			Type:     "type2",
+			Title:    "Another Title",
 			SubTitle: "the other subtitle",
 			Icon: output.AlfredIcon{
 				Filename: "bling.png",
 			},
 		},
 		output.Entry{
-			Uid: "foo3",
-			Arg: "/Users/rjkroege/tools/gopkg/src/github.com/rjkroege/leap/main.go",
-			Type: "type3"	,
-			Title: "Another Different Title",
+			Uid:      "foo3",
+			Arg:      "/Users/rjkroege/tools/gopkg/src/github.com/rjkroege/leap/main.go",
+			Type:     "type3",
+			Title:    "Another Different Title",
 			SubTitle: "a source file",
 			Icon: output.AlfredIcon{
 				Filename: "bling.png",
@@ -50,16 +48,15 @@ func NewStaticGenerator() output.Generator {
 // Presumption is that s is a query. So is probably a regexp.
 func (sg staticGenerator) Query(fn, qtype, suffix string) ([]output.Entry, error) {
 	s := fn
-	log.Printf("query: %s", s)
 
 	// basic approach: prefix match
 	result := make([]output.Entry, 0)
-	
-	for _, e := range(sg) {
+
+	for _, e := range sg {
 		if strings.HasPrefix(e.Title, s) {
 			result = append(result, e)
 		}
 	}
 
-	return result, nil	
+	return result, nil
 }
