38f6fc12dc9aaa7a7b050450e32ef8a908405dc6
Robert Kroeger <rjkroege@chromium.org>
2015-12-27 21:29:05 -0700

    Reload the index file when it changes
    Leap had a bug where server mode would not automatically detect that the
    index had changed and so searches for newly added or updated content
    would fail. This change causes the leap server to reload the index file
    if it detects that it has been altered.

--- server/interfaces.go
+++ server/interfaces.go
@@ -6,6 +6,8 @@ import (
 	"net/rpc"
  	"net/http"
 	"os"
+	"sync"
+	"time"
 
 	"github.com/rjkroege/leap/base"
 	"github.com/rjkroege/leap/output"
@@ -15,6 +17,9 @@ import (
 
 type Server struct {
 	search output.Generator
+	ftime time.Time
+	config *base.Configuration
+	lock sync.Mutex
 }
 
 type QueryBundle struct {
@@ -27,11 +32,20 @@ type QueryResult struct {
 	Entries []output.Entry
 }
 
-func BeginServing(config *base.Configuration)  {
-	// May wish to place self in new process group.	
+func getFileTime(filename string) time.Time {
+	finfo, err := os.Stat(filename)
+	if err != nil {
+		log.Fatal("couldn't open the index file: ", err)
+	}
+	return finfo.ModTime()
+}
 
-	// need to take index path from Configuration
-	state := &Server{ search.NewTrigramSearch(config.Indexpath, config.Prefixes) }
+func BeginServing(config *base.Configuration)  {
+	// Stash date of the index file that we actually use.
+	ftime := getFileTime(config.Indexpath)
+
+	// Need to take index path from Configuration.
+	state := &Server{ search: search.NewTrigramSearch(config.Indexpath, config.Prefixes), ftime: ftime, config: config }
 	rpc.Register(state)
 	rpc.HandleHTTP()
 
@@ -42,15 +56,30 @@ func BeginServing(config *base.Configuration)  {
 	http.Serve(l, nil)
 }
 
+func (t *Server) checkTimeAndUpdate() {
+	t.lock.Lock()
+	defer t.lock.Unlock()
+
+	// Compare the date with the stashed one.
+	ctime := getFileTime(t.config.Indexpath)
+	if t.ftime.Before(ctime) {
+		// Must reload the index file here.
+		// TODO(rjk): this needs to be in a lock?
+		t.ftime = ctime
+		t.search = search.NewTrigramSearch(t.config.Indexpath, t.config.Prefixes)
+	}
+}
+
 // Need to parse args myself.
 func (t *Server) Leap(query QueryBundle, resultBuffer *QueryResult) error {
+	t.checkTimeAndUpdate()
+
 	log.Println("go leap remoted: ", query)
 	entries, err := t.search.Query(query.Fn, query.Stype, []string{query.Suffix})
 	log.Println(entries)
 	*resultBuffer = QueryResult{entries}
 	return err
 }
-
 
 func (t *Server) Shutdown(ignored string, result *string) error {
 	log.Println("shutting down...")
