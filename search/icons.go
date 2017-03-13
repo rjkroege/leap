0cf4fd5e6fd97ab14e8fbff76ad3258b0fa264ae
Robert Kroeger <rjkroege@chromium.org>
2017-03-13 14:36:04 -0400

    Missing file added.
    Speculative fix neglected file. Added here.

---
+++ search/icons.go
@@ -0,0 +1,8 @@
+package search
+
+
+// determineIconString returns the name of a icon that we will
+// use for this file type.
+func determineIconString(name string) string {
+	return 	"cpp-icon-file-format.png"
+}
