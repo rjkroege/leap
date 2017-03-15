d97f146a110a16268207cbc0024ac9b804a5042e
Robert Kroeger <rjkroege@chromium.org>
2017-03-15 08:14:24 -0400

    Added support for using local icons.

--- search/icons.go
+++ search/icons.go
@@ -1,8 +1,28 @@
 package search
 
+import (
+	"path"
+)
 
 // determineIconString returns the name of a icon that we will
 // use for this file type.
 func determineIconString(name string) string {
-	return 	"cpp-icon-file-format.png"
+
+	switch path.Ext(name) {
+	case ".cpp", "cc":
+		return 	"/Applications/Xcode.app/Contents/Resources/c-plus-plus-source_Icon.icns"
+	case ".h", ".hpp":
+		return "/Applications/Xcode.app/Contents/Resources/c-header_Icon.icns"
+	case ".css":
+		return "/Applications/Safari.app/Contents/Resources/css.icns"
+	case ".js":
+		return "/Applications/Safari.app/Contents/Resources/js.icns"
+	case ".html", ".htm":
+		return "/Applications/Safari.app/Contents/Resources/html.icns"
+	case ".md", ".markdown":
+		return "/Applications/Marked 2.app/Contents/Resources/DocumentIcon.icns"
+	case ".go":
+		return "golang.icns"
+	}
+	return ""
 }
