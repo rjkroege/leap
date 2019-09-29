90183f08449a6db7911a982bb43f6d2d545c864e
Robert Kroeger <rjkroege@chromium.org>
2019-09-28 21:00:09 -0400

    Added legacy / upgrade config
    Added a new configuration scheme that can store multiple different
    per-project configurations, backwards compatability support and
    assorted tests.

--- base/config.go
+++ base/config.go
@@ -3,6 +3,7 @@ package base
 import (
 	"encoding/json"
 	"fmt"
+	"io"
 	"path/filepath"
 	"os"
 	"runtime"
@@ -19,35 +20,37 @@ type Configuration struct {
 	Indexpath string
 	Connect bool
 	Prefixes []string
+	newconfig *GlobalConfiguration
 }
 
 // The the new types.
 type Project struct {
-	host string  		`json:"host"`
-	indexpath string	`json:"indexpath"`
-	remote bool		`json:"remote"`
-	prefixes []string	`json:"prefixes"`
+	Host string  		`json:"host"`
+	Indexpath string	`json:"indexpath"`
+	Remote bool		`json:"remote"`
+	Prefixes []string	`json:"prefixes"`
 }
 
-type GobalConfiguration struct {
-	version int			`json:"version"`
-	currentproject string			`json:"currentproject"`
-	projects map[string]Project  	`json:"projects"`
+type GlobalConfiguration struct {
+	Version int			`json:"version"`
+	Currentproject string			`json:"currentproject"`
+	Projects map[string]*Project  	`json:"projects"`
 }
 
 // getLegacyConfiguration returns the legacy Configuration object corresponding
 // to the current project in the new style configuration.
-func (gc *GobalConfiguration) getLegacyConfiguration() (*Configuration, error) {
-	if _, ok := gc.projects[gc.currentproject] ; ok {
-		return nil, fmt.Errorf("no project corresponding to selected project %s", gc.currentproject)
+func (gc *GlobalConfiguration) getLegacyConfiguration() (*Configuration, error) {
+	if _, ok := gc.Projects[gc.Currentproject] ; !ok {
+		return nil, fmt.Errorf("no project corresponding to selected project %s", gc.Currentproject)
 	}
 	
-	np := gc.projects[gc.currentproject]
+	np := gc.Projects[gc.Currentproject]
 	return &Configuration{
-		Hostname:	np.host,
-		Indexpath:	np.indexpath,
-		Connect:		np.remote,
-		Prefixes:		np.prefixes,
+		Hostname:	np.Host,
+		Indexpath:	np.Indexpath,
+		Connect:		np.Remote,
+		Prefixes:		np.Prefixes,
+		newconfig:	gc,
 	}, nil
 }
 
@@ -78,18 +81,18 @@ func GetConfiguration(fp string) (*Configuration, error) {
 		return nil, err
 	}
 
-	decoder := json.NewDecoder(fd)
-	newstyleconfig := new(GobalConfiguration)
-	if err := decoder.Decode(newstyleconfig); err == nil && newstyleconfig.version == 1 {
-		return newstyleconfig.getLegacyConfiguration()
+	if ns, err := getNewConfig(fd); err == nil {
+		a, b := ns.getLegacyConfiguration()
+		return a, b
 	}
 
+	// New way didn't work so try decoding the old way.
 	if _, err := fd.Seek(0,0); err != nil {
 		return nil, fmt.Errorf("can't seek on config file %s because %v", fp, err)
 	}
 
-	// Try decoding the old way.
 	config := new(Configuration)
+	decoder := json.NewDecoder(fd)
 	if err := decoder.Decode(config); err != nil {
 		return nil, err
 	}
@@ -99,14 +102,85 @@ func GetConfiguration(fp string) (*Configuration, error) {
 	return config, nil
 }
 
-// TODO(rjk): I'm not going to worry about simultaneous mutation.
-func SaveConfiguration(config *Configuration, fp string) error {
+// getNewConfig reads the new style configuration data from disk.
+func getNewConfig(reader io.Reader) (*GlobalConfiguration, error) {
+	decoder := json.NewDecoder(reader)
+	newstyleconfig := new(GlobalConfiguration)
+	if err := decoder.Decode(newstyleconfig); err != nil {
+		return nil, err
+	}
+	if newstyleconfig.Version != 1 {
+		return nil, fmt.Errorf("unsupported newconfig version, can't decode")
+	}
+	return newstyleconfig, nil
+}
+
+// updateConfig makes a new-style configuration out of the old one.
+func updateConfig(oldconfig *Configuration) *GlobalConfiguration {
+	return &GlobalConfiguration{
+		Version: 1,
+		Currentproject: "default",
+		Projects: map[string]*Project {
+			"default": &Project{
+				Host: oldconfig.Hostname,
+				Indexpath: oldconfig.Indexpath,
+				Remote: oldconfig.Connect,
+				Prefixes: oldconfig.Prefixes,
+			},
+		},
+	}
+}
+
+// saveNewConfig writes a new config to disk or generates an error.
+func saveNewConfig(config *GlobalConfiguration, fp string) error {
 	fd, err := os.Create(fp)
 	if err != nil {
 		return err
 	}
 	defer fd.Close()
 
+	coder := json.NewEncoder(fd)
+	coder.SetIndent("", "	")
+	if err := coder.Encode(config); err != nil {
+		return err
+	}
+	return nil
+}
+
+// updateNewConfiguration will update config's embedded newconfig based
+// on changes made to the legacy Configuration payloads.
+func (config *Configuration) pushConfigIntoNew() {
+	nc := config.newconfig
+	
+	proj := nc.Projects[nc.Currentproject]
+
+	proj.Host = config.Hostname
+	proj.Indexpath = config.Indexpath
+	proj.Remote = config.Connect
+	proj.Prefixes = config.Prefixes
+}
+
+// TODO(rjk): I'm not going to worry about simultaneous mutation.
+// This code does not force conversion to the new format. I want
+// to do this manually via a top-level leap command to avoid
+// surprising myself.
+func SaveConfiguration(config *Configuration, fp string) error {
+	// We might have a new style config. If we do, we update that
+	// instead. Otherwise, we write out the old way.
+
+	fd, err := os.Create(fp)
+	if err != nil {
+		return fmt.Errorf("can't open file for config because %v", err)
+	}
+	defer fd.Close()
+
+	if _, err := getNewConfig(fd); err == nil {
+		// We have a new configuration so update that instead.
+		config.pushConfigIntoNew()
+		return saveNewConfig(config.newconfig, fp)
+	}
+
+	// We a old configuration. So update that.
 	coder := json.NewEncoder(fd)
 	if err := coder.Encode(config); err != nil {
 		return err
--- base/config_test.go
+++ base/config_test.go
@@ -1,6 +1,8 @@
 package base
 
 import (
+	"io/ioutil"
+	"os"
 	"path/filepath"
 	"reflect"
 	"testing"
@@ -20,6 +22,15 @@ func TestGetConfiguration(t *testing.T) {
 				Prefixes:  []string{"/Users/rjkroege/tools/gopkg/src"},
 			},
 		},
+		{
+			"leaprc_new",
+			Configuration{
+				Hostname:  "myhost",
+				Indexpath: "/home/gopher",
+				Connect:   false,
+				Prefixes:  []string{"/home/gopher/src"},
+			},
+		},
 	}
 
 	for _, p := range tt {
@@ -31,12 +42,123 @@ func TestGetConfiguration(t *testing.T) {
 			continue
 		}
 
-		if !reflect.DeepEqual(p.config, *conf) {
-			// TODO(rjk): use mods, use the pretty-printer, etc.
-			t.Errorf("didn't get desired value for %s: got %v, want %v", fp, conf, p.config)
+		econf := p.config
+		if got, want := conf.Hostname, econf.Hostname; got != want {
+			t.Errorf("%s wrong got %v want %v", "Hostname", got, want)
+		}
+		if got, want := conf.Indexpath, econf.Indexpath; got != want {
+			t.Errorf("%s wrong got %v want %v", "Indexpath", got, want)
+		}
+		if got, want := conf.Connect, econf.Connect; got != want {
+			t.Errorf("%s wrong got %v want %v", "Connect", got, want)
+		}
+		if got, want := conf.Prefixes, econf.Prefixes; !reflect.DeepEqual(got, want) {
+			t.Errorf("%s wrong got %v want %v", "Prefixes", got, want)
 		}
 	}
 }
 
-//func TestGetConfigurationMissing(t *testing.T) {
-//}
+func TestSaveNewConfig(t *testing.T) {
+	fp := filepath.Join("testdata", "leaprc_original")
+	oconf, err := GetConfiguration(fp)
+
+	nconf := updateConfig(oconf)
+
+	// TODO(rjk): make temp file
+	fd, err := ioutil.TempFile("", "leap_config")
+	if err != nil {
+		t.Fatalf("can't make a temp file because %v", err)
+	}
+	fd.Close()
+	name := fd.Name()
+	defer os.Remove(name)
+
+	if err := saveNewConfig(nconf, name); err != nil {
+		t.Fatalf("can't write a config because %v", err)
+	}
+
+	// Should be able to get legacy from upgraded.
+	conf, err := GetConfiguration(name)
+	if err != nil {
+		t.Fatalf("can't read a new-style config because %v", err)
+	}
+
+	econf := Configuration{
+		Hostname:  "",
+		Indexpath: "/Users/rjkroege/.csearchindex",
+		Connect:   false,
+		Prefixes:  []string{"/Users/rjkroege/tools/gopkg/src"},
+	}
+
+	if got, want := conf.Hostname, econf.Hostname; got != want {
+		t.Errorf("%s wrong got %v want %v", "Hostname", got, want)
+	}
+	if got, want := conf.Indexpath, econf.Indexpath; got != want {
+		t.Errorf("%s wrong got %v want %v", "Indexpath", got, want)
+	}
+	if got, want := conf.Connect, econf.Connect; got != want {
+		t.Errorf("%s wrong got %v want %v", "Connect", got, want)
+	}
+	if got, want := conf.Prefixes, econf.Prefixes; !reflect.DeepEqual(got, want) {
+		t.Errorf("%s wrong got %v want %v", "Prefixes", got, want)
+	}
+}
+
+func TestUpdateNewConfig(t *testing.T) {
+	fd, err := ioutil.TempFile("", "leap_config")
+	if err != nil {
+		t.Fatalf("can't make a temp file because %v", err)
+	}
+	name := fd.Name()
+	fd.Close()
+	defer os.Remove(name)
+
+	// Copy leaprc_new to temp file
+	fp := filepath.Join("testdata", "leaprc_new")
+	contents, err := ioutil.ReadFile(fp)
+	if err != nil {
+		t.Fatalf("can't read starter config because %v", err)
+	}
+	if err := ioutil.WriteFile(name, contents, 0644); err != nil {
+		t.Fatalf("can't read starter config because %v", err)
+	}
+
+	oconf, err := GetConfiguration(name)
+	if err != nil {
+		t.Fatalf("can't read starter config because %v", err)
+	}
+
+	oconf.Hostname = "pinkelephant"
+	oconf.Indexpath = "/home/augie"
+	oconf.Connect = true
+	oconf.Prefixes = []string{"/home/augie/go/src"}
+
+	if err := SaveConfiguration(oconf, name); err != nil {
+		t.Fatalf("can't write starter config because %v", err)
+	}
+
+	conf, err := GetConfiguration(name)
+	if err != nil {
+		t.Fatalf("can't read modified config because %v", err)
+	}
+
+	econf := Configuration{
+		Hostname:  "pinkelephant",
+		Indexpath: "/home/augie",
+		Connect:   true,
+		Prefixes:  []string{"/home/augie/go/src"},
+	}
+
+	if got, want := conf.Hostname, econf.Hostname; got != want {
+		t.Errorf("%s wrong got %v want %v", "Hostname", got, want)
+	}
+	if got, want := conf.Indexpath, econf.Indexpath; got != want {
+		t.Errorf("%s wrong got %v want %v", "Indexpath", got, want)
+	}
+	if got, want := conf.Connect, econf.Connect; got != want {
+		t.Errorf("%s wrong got %v want %v", "Connect", got, want)
+	}
+	if got, want := conf.Prefixes, econf.Prefixes; !reflect.DeepEqual(got, want) {
+		t.Errorf("%s wrong got %v want %v", "Prefixes", got, want)
+	}
+}
